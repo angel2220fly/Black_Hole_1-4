15efa0d18fbb94abe0ef782deaf81df9de53d4fc
JURJIUS PACALOVAS <56896464+angel2220fly@users.noreply.github.com>
2024-05-14 11:19:17 +0100

    Update Black_Hole_3.py

--- Black_Hole_3.py
+++ Black_Hole_3.py
@@ -226,7 +226,7 @@ class compression:
                                                                     while block<long_F:
                                                                         INFO_A=INFO[block:block+En]
                                                                         if En1!=0:
-                                                                                INFO_A1=INFO[block+En:block+En+En1-En3]
+                                                                                INFO_A1=INFO[block+En:block+En+En1]
                                                                         longl=len(INFO_A)
                                                                         
                                                                         Counts=int(INFO_A,2)
@@ -293,9 +293,9 @@ class compression:
                                                                                    
                                                                             if C3!=1:
                                                                                    Z5="011"+C1+C
-                                                                                   if En1!=0 and block+En1+En-En3<=long_F:
+                                                                                   if En1!=0 and block+En1+En<=long_F:
                                                                                            Z5="011"+C1+C+INFO_A1
-                                                                                           block+=En1-En3
+                                                                                           block+=En1
                                                                                    #print(Z5) 
                                                                                        
                                                                                    
@@ -393,7 +393,7 @@ class compression:
                                                                 CL1=format(longl,W)        
                                                                 CL2=format(En,'013b')
                                                                 CL3=format(En1,'013b')
-                                                                CL4=format(En3,'03b')
+                                                                
                                                                
                                                                 #print(N3)
                                                                                                                          
@@ -469,9 +469,7 @@ class compression:
                                                             
                                     INFO=Extract
 
-                                    En3=int(INFO[:3],2)
-                                        #print(longl)
-                                    INFO=INFO[3:]
+                                
                                     
                                     En2=int(INFO[:13],2)
                                         #print(longl)
@@ -600,11 +598,11 @@ class compression:
                                                                 Z2Z=format(E,C)
                                                                 if En2!=0:
                                                                         CAll=0
-                                                                        CAll=int(block)+int(En2-En3)
+                                                                        CAll=int(block)+int(En2)
                                                                         if CAll<=long_F:
-                                                                            EB1=INFO[block:block+En2-En3]
+                                                                            EB1=INFO[block:block+En2]
                                                                             ZE=ZE+EB1
-                                                                            block+=En2-En3
+                                                                            block+=En2
                                                                             C9=1
                                                                             
                                                             else:
